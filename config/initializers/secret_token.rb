5b55b50eb2138ef91e660f146f00b030e62f1077
Carlos Cruzado <ccruzadobejarano@gmail.com>
2012-01-30 21:32:08 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DEWchat2::Application.config.secret_token = '10491e0218fc1e3e73c19436d18250a29920bec3678f819e8d598d1e596c68438206dc632bde5249bcf3410ba0df2191af3d43828775e719f6a1fdf37ff0509d'
